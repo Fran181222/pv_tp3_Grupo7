d609e330089cb090411c5dc2733fdac5ba7f3bf8
Rush <gabrielroel777@gmail.com>
2025-05-09 01:18:25 -0300

    ejercicio 5

---
+++ src/ejercicio_adicional_js/punto5.js
@@ -0,0 +1,9 @@
+const productos = [
+    {descripcion: "Auriculares", precio: 49000}, 
+    {descripcion: "Teclado", precio: 30000},
+    {descripcion: "Mouse", precio: 20000},
+    {descripcion: "Monitor", precio: 100000},
+    {descripcion: "Parlante", precio: 15000},
+]
+
+productos.push({descripcion: "Celular", precio: 80000});
