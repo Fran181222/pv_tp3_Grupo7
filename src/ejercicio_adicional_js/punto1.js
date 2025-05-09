cf89339f3b357f01ab84b59f115a8685c991108b
Ruancas <ruancas025@gmail.com>
2025-05-09 01:29:41 -0300

    punto1-2_arreglado

--- src/ejercicio_adicional_js/punto1.js
+++ src/ejercicio_adicional_js/punto1.js
@@ -1,12 +1,12 @@
 const productos = [
-    {descripcion: "Auriculares", precio: 49000}, 
-    {descripcion: "Teclado", precio: 30000},
-    {descripcion: "Mouse", precio: 20000},
-    {descripcion: "Monitor", precio: 100000},
-    {descripcion: "Parlante", precio: 15000},
+    {descripcion: "Auriculares", precio: 49000.00}, 
+    {descripcion: "Teclado", precio: 30000.00},
+    {descripcion: "Mouse", precio: 20000.00},
+    {descripcion: "Monitor", precio: 100000.00},
+    {descripcion: "Parlante", precio: 15000.00},
 ]
 
 //Mostrar en consola cada producto en el formato: "Producto: [descripción] - Precio:$[precio]", usando forEach.
 productos.forEach (producto=>{
-    console.log (`Producto: ${producto.descripcion} Precio: ${producto.precio}`)
+    console.log (`Producto: ${producto.descripcion} Precio: ${producto.precio.toFixed(2)}`)
 })
--- src/ejercicio_adicional_js/punto2.js
+++ src/ejercicio_adicional_js/punto2.js
@@ -1,12 +1,14 @@
 const productos = [
-    {descripcion: "Auriculares", precio: 49000}, 
-    {descripcion: "Teclado", precio: 30000},
-    {descripcion: "Mouse", precio: 20000},
-    {descripcion: "Monitor", precio: 100000},
-    {descripcion: "Parlante", precio: 15000},
+    {descripcion: "Auriculares", precio: 49000.00}, 
+    {descripcion: "Teclado", precio: 30000.00},
+    {descripcion: "Mouse", precio: 20000.00},
+    {descripcion: "Monitor", precio: 100000.00},
+    {descripcion: "Parlante", precio: 15000.00},
 ]
 
 //Crear un nuevo array con los productos cuyo precio sea mayor a $20, usando filter
 const productosFiltrados = productos.filter(producto => producto.precio >20000);
 
-console.log(productosFiltrados);
+productosFiltrados.forEach (producto=>{
+    console.log (`Producto: ${producto.descripcion} Precio: ${producto.precio.toFixed(2)}`)
+})
